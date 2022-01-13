76c744846cacb1237fef91f0344f53322c36e7b2
Mario Ceron Charry <mario.ceron@mceron.com>
2022-01-13 09:20:45 -0500

    Initial commit

---
+++ portal/lib/local_settings.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2022. CQ Inversiones SAS. Todos los derechos reservados. All rights reserved.
+#  Este archivo se considera confidencial y no se encuentra licenciado bajo ninguna licencia de código abierto.
+#  Se prohibe explicitamente su uso en proyectos o sistemas diferentes a los desarrollados por CQ Inversiones SAS y/o
+#  aquellos en los que se autorice de manera explícita y por escrito su uso.
+#  Cualquier utilización indebida del código o fragmentos del código contenido en este archivo, se encuentra penalizado
+#  por las leyes de Colombia, y a nivel internacional de acuerdo a los tratados existentes sobre
+#  Protección de Derechos de Autor.
+
+# ********************************************************
+# Project: cam-portal
+# IDE: PyCharm
+# Developed by: macercha
+# File Name: local_settings.py
+# Date: 13/01/22 - 8:41 AM
+# *******************************************************
+
+PORTAL_DEBUG = True
+PORTAL_SECRET_KEY = "#XYCFuv9h3*z=K6MbH@=N3$p*@p_zLWV#_8JMV2=$LN6PGVMLD5pfgsAUyRh8m4s"
+PORTAL_MAIL_HOST = "smtp.office365.com"
+PORTAL_MAIL_PORT = "587"
+PORTAL_MAIL_USER = "meerkad@meerkad.net"
+PORTAL_MAIL_PASSWORD = "79klmht23*"
+PORTAL_DB_HOST = "localhost"
+PORTAL_DB_PORT = "3306"
+PORTAL_DB_SCHEMA = "portal_cam"
+PORTAL_DB_USER = "portal_cam"
+PORTAL_DB_PASSWORD = "SmD$$75Lc*"
+PORTAL_DB_MONGO_HOST = "localhost"
+PORTAL_DB_MONGO_PORT = 57077
+PORTAL_DB_MONGO_SCHEMA = "portal_cam"
+PORTAL_DB_MONGO_USER = "portal_cam"
+PORTAL_DB_MONGO_PASSWORD = "SmD$$75Lc*"
+PORTAL_DB_MONGO_AUTH = "admin"
+
